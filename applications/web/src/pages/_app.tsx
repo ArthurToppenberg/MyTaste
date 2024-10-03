308248837069f5f240f8112c221eafe1ee9ea948
Arthur Toppenberg <Arthur.toppenberg@gmail.com>
2024-10-03 18:41:41 +0200

    add vercel analytincs support

--- applications/web/src/pages/_app.tsx
+++ applications/web/src/pages/_app.tsx
@@ -1,22 +1,24 @@
 import type { AppProps } from 'next/app';
 import AuthProvider from "@packages/authProvider";
+import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     return (
-        <AuthProvider
-            apiPath={`${process.env.NEXTAUTH_URL}/api`}
-            localSaveToken={(token: string) => {
-                localStorage.setItem('token', token);
-            }}
-            localDeleteToken={() => {
-                localStorage.removeItem('token');
-                return true;
-            }}
-            localGetToken={() => {
-                return localStorage.getItem('token') || '';
-            }}>
-            <Component {...pageProps} />
-        </AuthProvider>
+            <AuthProvider
+                apiPath={`${process.env.NEXTAUTH_URL}/api`}
+                localSaveToken={(token: string) => {
+                    localStorage.setItem('token', token);
+                }}
+                localDeleteToken={() => {
+                    localStorage.removeItem('token');
+                    return true;
+                }}
+                localGetToken={() => {
+                    return localStorage.getItem('token') || '';
+                }}>
+                <Component {...pageProps} />
+                <Analytics />
+            </AuthProvider>
     );
 }
 
