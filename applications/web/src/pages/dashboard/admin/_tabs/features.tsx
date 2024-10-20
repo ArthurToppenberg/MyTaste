6a290426660680ec10e113d28d7414ae3c0ad489
Arthur Toppenberg <Arthur.toppenberg@gmail.com>
2024-10-20 02:15:04 +0200

    assasinate stupid tab header since its usless and no one likes it

--- applications/web/src/pages/dashboard/admin/_tabs/features.tsx
+++ applications/web/src/pages/dashboard/admin/_tabs/features.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import TabHeader from '../../_components/tabHeader';
 import style from '@/styles/dashboardTab.module.css';
 import CustomTable from '../_components/table';
 import TableToolbar from '../_components/tableToolbar';
@@ -19,46 +18,24 @@ const Features: React.FC = () => {
     }
 
     return (
-        <>
-            <TabHeader title='Features' />
-
-            <div className={style.container}>
-                <TableToolbar addButtonName='Add Feature' />
-                <CustomTable
-                    columns={[
-                        { name: 'Name', key: 'name' },
-                        { name: 'Min', key: 'min' },
-                        { name: 'Max', key: 'max' },
-                    ]}
-                    data={[
-                        { name: 'Feature 1', min: 0, max: 10 },
-                        { name: 'Feature 2', min: 0, max: 10 },
-                        { name: 'Feature 3', min: 0, max: 10 },
-                        { name: 'Feature 1', min: 0, max: 10 },
-                        { name: 'Feature 2', min: 0, max: 10 },
-                        { name: 'Feature 3', min: 0, max: 10 },
-                        { name: 'Feature 1', min: 0, max: 10 },
-                        { name: 'Feature 2', min: 0, max: 10 },
-                        { name: 'Feature 3', min: 0, max: 10 },
-                        { name: 'Feature 1', min: 0, max: 10 },
-                        { name: 'Feature 2', min: 0, max: 10 },
-                        { name: 'Feature 3', min: 0, max: 10 },
-                        { name: 'Feature 1', min: 0, max: 10 },
-                        { name: 'Feature 2', min: 0, max: 10 },
-                        { name: 'Feature 3', min: 0, max: 10 },
-                        { name: 'Feature 1', min: 0, max: 10 },
-                        { name: 'Feature 2', min: 0, max: 10 },
-                        { name: 'Feature 3', min: 0, max: 10 },
-                        { name: 'Feature 1', min: 0, max: 10 },
-                        { name: 'Feature 2', min: 0, max: 10 },
-                        { name: 'Feature 3', min: 0, max: 10 },
-                    ]}
-                    editAction={handleEdit}
-                    deleteAction={handleDelete}
-                    saveAction={handleSave}
-                />
-            </div>
-        </>
+        <div className={style.container}>
+            <TableToolbar addButtonName='Add Feature' />
+            <CustomTable
+                columns={[
+                    { name: 'Name', key: 'name' },
+                    { name: 'Min', key: 'min' },
+                    { name: 'Max', key: 'max' },
+                ]}
+                data={[
+                    { name: 'Feature 1', min: 0, max: 10 },
+                    { name: 'Feature 2', min: 0, max: 10 },
+                    { name: 'Feature 3', min: 0, max: 10 },
+                ]}
+                editAction={handleEdit}
+                deleteAction={handleDelete}
+                saveAction={handleSave}
+            />
+        </div>
     );
 };
 
